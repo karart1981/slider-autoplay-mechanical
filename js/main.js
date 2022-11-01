e000eaf9013ca8649911d982c54af850e02499af
karart1981 <karapart1981@gmail.com>
2022-11-01 18:25:09 +0400

    I created fide-in/fide-out button

--- js/main.js
+++ js/main.js
@@ -48,21 +48,24 @@ function titleImage(){
 const autoplay = document.querySelector("#autoplay");
 autoplay.addEventListener("click", function(){
 	let slideIndex = 0;
-	showSlides();
-	
-   
+showSlides();
 
-	function showSlides(n) {
-	  let i;
-	  let slides = document.getElementsByClassName("mySlides");
-	  for (i = 0; i < slides.length; i++) {
-		slides[i].style.display = "none";
-	  }
-	  slideIndex++;
-	  if (slideIndex > slides.length) {slideIndex = 1;}
-	  slides[slideIndex-1].style.display = "block";
-	  setTimeout(showSlides, 4000); // Change image every 4 seconds
-	}
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
 });
 
 /* Carousel*/ 
